6d4c74d6f1b1b7b2961eea19fef59768c12ffabf
rivkadarshan <rivka3188@gmail.com>
2024-04-10 14:48:14 +0300

    link content

--- src/app/components/link-content/link-content.component.ts
+++ src/app/components/link-content/link-content.component.ts
@@ -5,6 +5,33 @@ import { Component } from '@angular/core';
   templateUrl: './link-content.component.html',
   styleUrls: ['./link-content.component.css']
 })
-export class LinkContentComponent {
 
-}
+
+export class LinkContentComponent {
+  showEmail: boolean = false;
+  showPhone: boolean = false;
+  showWhatsapp: boolean = false;
+
+  showPhoneInfo() {
+    this.showPhone = true;
+  }
+
+  hidePhoneInfo() {
+    this.showPhone = false;
+  }
+
+  showEmailInfo() {
+    this.showEmail = true;
+  }
+
+  hideEmailInfo() {
+    this.showEmail = false;
+  }
+
+  showWhatsappInfo() {
+    this.showWhatsapp = true;
+  }
+  hideWhatsappInfo() {
+    this.showWhatsapp = false;
+  }
+ }
