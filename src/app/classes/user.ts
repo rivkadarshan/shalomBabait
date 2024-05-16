68cc24a46372d4bd780fccce4542fad6126d318a
rivkadarshan <rivka3188@gmail.com>
2024-05-16 15:47:15 +0300

    login component and user class

---
+++ src/app/classes/user.ts
@@ -0,0 +1,13 @@
+export class User {
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public tz: string,
+        public phone: string,
+        public city:string,
+        public addres:string,
+        public mail:string,
+        public password:string,
+        public isGetUpdates:boolean
+      ) {} 
+}
--- src/app/components/log-in/log-in.component.ts
+++ src/app/components/log-in/log-in.component.ts
@@ -1,3 +1,15 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-log-in',
+//   templateUrl: './log-in.component.html',
+//   styleUrls: ['./log-in.component.css']
+// })
+// export class LogInComponent {
+//   enrollmentFormDisplayed: boolean = true;
+//   connectionFormDisplayed: boolean = false;
+//   forgatPassword:boolean=false
+// }
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,5 +19,25 @@ import { Component } from '@angular/core';
 })
 export class LogInComponent {
   enrollmentFormDisplayed: boolean = true;
-  loginFormDisplayed: boolean = false;
+  connectionFormDisplayed: boolean = false;
+  forgatPassword: boolean = false;
+
+  showEnrollmentForm() {
+    this.enrollmentFormDisplayed = true;
+    this.connectionFormDisplayed = false;
+    this.forgatPassword = false;
+  }
+
+  showConnectionForm() {
+    this.enrollmentFormDisplayed = false;
+    this.connectionFormDisplayed = true;
+    this.forgatPassword = false;
+  }
+
+  showForgotPassword() {
+    this.enrollmentFormDisplayed = false;
+    this.connectionFormDisplayed = false;
+    this.forgatPassword = true;
+  }
 }
+
