d7600b719b617b3c6c550896bbbf08ba7ffe167d
sara-kra <sarakra5102@gmail.com>
2025-09-16 11:06:38 +0300

    Add calling to server

--- src/app/components/personal-area/therapist/patient-list/patient-list.component.ts
+++ src/app/components/personal-area/therapist/patient-list/patient-list.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-patient-list',
   templateUrl: './patient-list.component.html',
   styleUrls: ['./patient-list.component.css']
 })
-export class PatientListComponent {
-  patients = [
-    { name: 'יוסי כהן', id: '123456789' },
-    { name: 'מיכל לוי', id: '987654321' },
-    { name: 'דוד ישראלי', id: '456789123' },
-    { name: 'שרה פישר', id: '321654987' },
-    { name: 'אורן ברק', id: '159753456' },
-  ];
+export class PatientListComponent implements OnInit {
+  patients: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getPatients();
+  }
+
+  getPatients() {
+    const therapistId = '123'; 
+    this.http.get<any[]>(`http://localhost:3000/api/patients/byTherapist/${therapistId}`)
+      .subscribe(data => {
+        this.patients = data;
+      });
+  }
 
   viewPatientDetails(patient: any) {
     console.log(patient);
