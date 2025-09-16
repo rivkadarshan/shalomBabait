9763fa2360a792ad58a8ff1049d2e0b32a0f41c9
Rivka Darshan <rivka3188@gmail.com>
2025-09-16 11:08:12 +0300

    merge some branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HeaderComponent } from "./components/marketing/header/header.component"
 import { AdvertisingComponent } from "./components/marketing/advertising/advertising.component";
 import { AboutComponent } from "./components/marketing/about/about.component";
 import { DonateButtonComponent } from "./components/marketing/donate-button/donate-button.component";
+
 import { FooterComponent } from "./components/marketing/footer/footer.component";
 import { ListOfActivitiesComponent } from './components/marketing/list-of-activities/list-of-activities.component';
 import { WeNumbersComponent } from "./components/marketing/we-numbers/we-numbers.component";
@@ -32,7 +33,6 @@ import { MatTableModule } from '@angular/material/table';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { SignUpForCourseComponent } from './components/marketing/sign-up-for-course/sign-up-for-course.component';
 import { HomeComponent } from './components/marketing/home/home.component';
-import { DetilsContactComponent } from './components/marketing/detils-contact/detils-contact.component';
 import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
 import { LogInComponent } from './components/marketing/log-in/log-in.component';
 import { ConsultationMeetingComponent } from './components/consultation-meeting/consultation-meeting.component';
@@ -40,9 +40,7 @@ import { AdminDashboardComponent } from './components/personal-area/admin/admin-
 import { SecretaryDashboardComponent } from './components/personal-area/secretary/secretary-dashboard/secretary-dashboard.component';
 import { TherapistDashboardComponent } from './components/personal-area/therapist/therapist-dashboard/therapist-dashboard.component';
 import { PatientDashboardComponent } from './components/personal-area/patient/patient-dashboard/patient-dashboard.component';
-import {LinkContentComponent} from './components/marketing/link-content/link-content.component';
-import { ConferenceComponent } from './components/marketing/conference/conference.component';
-import { SafeUrlPipe } from './pipes/safe-url.pipe'
+import { LinkContentComponent } from './components/marketing/link-content/link-content.component';
 
 @NgModule({
     declarations: [
@@ -56,18 +54,16 @@ import { SafeUrlPipe } from './pipes/safe-url.pipe'
         WeNumbersComponent,
         ContactComponent,
         TheySayAboutUsComponent,
-        LinkContentComponent,
         SignUpForCourseComponent,
         HomeComponent,
-        DetilsContactComponent,
         RegistrationFormComponent,
         LogInComponent,
         ConsultationMeetingComponent,
         AdminDashboardComponent,
         SecretaryDashboardComponent,
         TherapistDashboardComponent,
-        ConferenceComponent,
-        SafeUrlPipe,
+        PatientDashboardComponent,
+        LinkContentComponent,
     ],
     imports: [
         BrowserModule,
